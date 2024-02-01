cb3c4f97c8e895d353952eb6c4b5e41cc528df22
Tatiana Tenizbaeva <tatanatenizbaeva@MacBook-Air-Tatana.local>
2024-02-01 14:00:56 +0300

    1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list')
 
-// @todo: Функция создания карточки
+
+initialCards.forEach(function(item) {
+
+const cardTemplate = document.querySelector('#card-template').content;
+const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+card.querySelector('.card__image').src = item.link;
+card.querySelector('.card__title').textContent = item.name;
+card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+placesList.append(card)
+})
+
+
+
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+    const cardItem = document.querySelector('.card__delete-button').closest('.places__item');
+    cardItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
